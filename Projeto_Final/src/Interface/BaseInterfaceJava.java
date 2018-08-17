fbc647ab5eeb6e50a71166f896bc659845a38535
ErickBlack <erickdejesus44111@gmail.com>
2018-08-17 10:25:02 -0300

    Base da Interface
    17/08/2018

---
+++ Projeto_Final/src/Interface/BaseInterfaceJava.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interface;
+
+/**
+ *
+ * @author Guilherme Bialas
+ */
+public interface BaseInterfaceJava {
+
+    void gerarTela();
+
+    void adicionarComponentes();
+
+    void gerarLocalizacoes();
+
+    void gerarDimensoes();
+}
