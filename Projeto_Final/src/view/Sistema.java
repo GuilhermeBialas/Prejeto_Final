65ac8464c285a046201cfaa07ab60e4776226ba8
marcio <mpschiehl2outlook.com>
2018-08-23 08:12:49 -0300

    correção
    23/08/2018

--- Projeto_Final/src/view/Sistema.java
+++ Projeto_Final/src/view/Sistema.java
@@ -22,8 +22,9 @@ import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
  *
  * @author Márcio Pedro Schiehl
  */
-public class Sistema implements BaseInterfaceJava{
-    public Sistema (){
+public class Sistema implements BaseInterfaceJava {
+
+    public Sistema() {
         gerarTela();
         trocaIcone();
         instanciarComponentes();
@@ -34,28 +35,24 @@ public class Sistema implements BaseInterfaceJava{
         acaoBotaoEstoque();
         acaoBotaoVenda();
         acaoBotaoSair();
-       jFrame.setVisible(true);
+        jFrame.setVisible(true);
     }
-    public static void main(String[] args) {
-        new Sistema();
-    }
-    private JFrame jFrame;
-   private JButton jButtonCadastro, jButtonEstoque, jButtonVenda, jButtonSair;
 
-    /*@Override
+    private JFrame jFrame;
+    private JButton jButtonCadastro, jButtonEstoque, jButtonVenda, jButtonSair;
+
+    @Override
     public void instanciarComponentes() {
-       jButtonCadastro = new JButton("Cadastro");
+        jButtonCadastro = new JButton("Cadastro");
         jButtonEstoque = new JButton("Estoque");
         jButtonVenda = new JButton("Venda");
         jButtonSair = new JButton("Sair");
-    }*/
+    }
 
-    
-   
     @Override
     public void gerarTela() {
         jFrame = new JFrame("nome do sistema");
-        jFrame.setSize(800,600);
+        jFrame.setSize(800, 600);
         jFrame.setLocationRelativeTo(null);
         jFrame.setLayout(null);
         jFrame.setResizable(false);
@@ -64,25 +61,24 @@ public class Sistema implements BaseInterfaceJava{
 
     @Override
     public void gerarLocalizacoes() {
-        jButtonCadastro.setLocation(10,10);
-        jButtonEstoque.setLocation(120,10);
-        jButtonVenda.setLocation(240,10);
-        jButtonSair.setLocation(360,10);
+        jButtonCadastro.setLocation(10, 10);
+        jButtonEstoque.setLocation(120, 10);
+        jButtonVenda.setLocation(240, 10);
+        jButtonSair.setLocation(360, 10);
     }
 
     @Override
     public void gerarDimensoes() {
-        jButtonCadastro.setSize(100,50);
-        jButtonEstoque.setSize(100,50);
-        jButtonSair.setSize(100,50);
-        jButtonVenda.setSize(100,50);
+        jButtonCadastro.setSize(100, 50);
+        jButtonEstoque.setSize(100, 50);
+        jButtonSair.setSize(100, 50);
+        jButtonVenda.setSize(100, 50);
     }
 
-
     private void trocaIcone() {
-       URL url = this.getClass().getResource("/icones/air-filter.png");
+        URL url = this.getClass().getResource("/icones/air-filter.png");
         Image imagemTitulo = Toolkit.getDefaultToolkit().getImage(url);
-       jFrame.setIconImage(imagemTitulo);
+        jFrame.setIconImage(imagemTitulo);
     }
 
     private void acaoBotaoCadastro() {
@@ -114,7 +110,7 @@ public class Sistema implements BaseInterfaceJava{
             public void actionPerformed(ActionEvent e) {
                 JOptionPane.showMessageDialog(null, "Em Construção");
                 //new Vendas();
-               
+
             }
         });
     }
@@ -124,17 +120,9 @@ public class Sistema implements BaseInterfaceJava{
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                 System.exit(0);
+                System.exit(0);
             }
         });
-    }
-
-    @Override
-    public void instaciarComponentes() {
-        jButtonCadastro = new JButton("Cadastro");
-        jButtonEstoque = new JButton("Estoque");
-        jButtonVenda = new JButton("Venda");
-        jButtonSair = new JButton("Sair");
     }
 
     @Override
@@ -145,7 +133,4 @@ public class Sistema implements BaseInterfaceJava{
         jFrame.add(jButtonSair);
     }
 
-
-    
-    
 }
