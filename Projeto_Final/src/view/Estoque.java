4ad46427be78d406668278594bed52d85142df8e
Carlosaugustob <38558653+Carlosaugustob@users.noreply.github.com>
2018-08-23 10:12:10 -0300

    CRIANDO A INTERFACE DO ESTOQUE
    SALVANDO AS INCREMENTAÇÕES DA VIEW, e acresentando a tela começo.

--- Projeto_Final/src/view/Estoque.java
+++ Projeto_Final/src/view/Estoque.java
@@ -1,14 +1,70 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package view;
+
+import Interface.BaseInterfaceJava;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 /**
  *
- * @author Alunos
+ * @author Alunos Carlos Augusto Borges
  */
-public class Estoque {
+     public class Estoque implements BaseInterfaceJava {
+         
+           private JFrame jFrame;
+    private JLabel jLabelQuantidade, jLabelValor, jLabelDescricao, jLabelAplicacao,
+            jLabelUnidadeDeMedida, jLabelLocalizacao, jLabelValorUnitario, jLabelStatusPeca, jLabelPeso;
+    private JTextField jTextFieldQuantiade, jTextFieldValor, jTextFieldDescricao, jTextFieldAplicacao, jTextFieldUnidadeDeMedida,
+            jTextFieldLocalizacao, jTextFieldValorUnitario, jTextFieldStatusPeca, jTextFieldPeso;
+    private JButton jButtonSalvar, jButtonExcluir, jButtonAdicionar;
+         
+    public void Estoque(){
+           
+        gerarTela();
+        trocaIcone();
+        instanciarComponentes();
+        adicionarComponentes();
+        gerarLocalizacoes();
+        gerarDimensoes();
+        acaoBotaoCadastro();
+        acaoBotaoEstoque();
+        acaoBotaoVenda();
+        acaoBotaoSair();
+       jFrame.setVisible(true);
+        
+    }
+
+  
+    public void instaciarComponentes() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void gerarTela() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
     
-}
+    public void adicionarComponentes() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+    public void gerarLocalizacoes() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+    public void gerarDimensoes() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+    public void instanciarComponentes() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+        
+      }
+     
+
