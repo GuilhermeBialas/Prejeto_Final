cb020faf4f32d9593e65070577e9eacc64589a13
Guilherme Bialas <38558633+GuilhermeBialas@users.noreply.github.com>
2018-08-30 10:39:16 -0300

    Estoque
    Falta add os componentes e add as localizações

--- Projeto_Final/src/view/Estoque.java
+++ Projeto_Final/src/view/Estoque.java
@@ -1,11 +1,14 @@
 package view;
 
 import Interface.BaseInterfaceJava;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 import static javax.swing.WindowConstants.DISPOSE_ON_CLOSE;
+
 
 /**
  *
@@ -19,6 +22,7 @@ public class Estoque implements BaseInterfaceJava {
     private JLabel jLabelId, jLabelQuantidade, jLabelStatus, jLabelCategoria, jLabelProduto;
     private JTextField jTextFieldId, jTextFieldQuantidade, jTextFieldProduto;
     private JButton jButtonSair;
+    private JRadioButton jRadioButtonNovo, jRadioButtonSemiNovo;
 
     public Estoque() {
         instanciarComponentes();
@@ -52,20 +56,26 @@ public class Estoque implements BaseInterfaceJava {
         jFrameEstoque.add(jTextFieldId);
         jFrameEstoque.add(jTextFieldQuantidade);
         
-
+        //JRadioButton's
+        jFrameEstoque.add(jRadioButtonNovo);
+        jFrameEstoque.add(jRadioButtonSemiNovo);
     }
 
     @Override
     public void gerarLocalizacoes() {
         //jLabel's
         jLabelId.setLocation(20, 20);
-        jLabelQuantidade.setLocation(20,50);
-        jLabelStatus.setLocation(125,20);
+        jLabelQuantidade.setLocation(20,70);
+        jLabelStatus.setLocation(140,20);
         
 
         //jTextField's
-        jTextFieldId.setLocation(120, 20);
-        jTextFieldQuantidade.setLocation(120,50);
+        jTextFieldId.setLocation(20, 40);
+        jTextFieldQuantidade.setLocation(20,90);
+        
+        //JRadioButton's
+        jRadioButtonNovo.setLocation(260,20);
+        jRadioButtonSemiNovo.setLocation(260,50);
     }
 
     @Override
@@ -78,6 +88,10 @@ public class Estoque implements BaseInterfaceJava {
         //jTextField's
         jTextFieldId.setSize(100, 20);
         jTextFieldQuantidade.setSize(100,20);
+        
+        //JRadioButton's
+        jRadioButtonNovo.setSize(20,20);
+        jRadioButtonSemiNovo.setSize(20,20);
     }
 
     @Override
@@ -86,9 +100,14 @@ public class Estoque implements BaseInterfaceJava {
         jLabelId = new JLabel("Número");
         jLabelQuantidade = new JLabel("Quantidade");
         jLabelStatus = new JLabel("Status");
+        
         //jTextField's
         jTextFieldId = new JTextField("");
         jTextFieldQuantidade = new JTextField("");
+        
+        //JRadioButton's
+        jRadioButtonNovo = new JRadioButton();
+        jRadioButtonSemiNovo = new JRadioButton();
     }
 
 }
