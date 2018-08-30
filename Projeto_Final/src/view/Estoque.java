bb17398fb85936b6469407bb7a6f5d642d0c9bae
Guilherme Bialas <38558633+GuilhermeBialas@users.noreply.github.com>
2018-08-30 09:43:37 -0300

    Estoque
    30/08/2018

--- Projeto_Final/src/view/Estoque.java
+++ Projeto_Final/src/view/Estoque.java
@@ -1,9 +1,7 @@
 package view;
 
 import Interface.BaseInterfaceJava;
-import javafx.scene.control.ComboBox;
 import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
@@ -21,7 +19,7 @@ public class Estoque implements BaseInterfaceJava {
     private JLabel jLabelId, jLabelQuantidade, jLabelStatus, jLabelCategoria, jLabelPesquisa;
     private JTextField jTextFieldId, jTextFieldQuantidade, jTextFieldStatus, jTextFieldPesquisa;
     private JButton jButtonSair;
-    private ComboBox jComboBoxCategoria;
+
 
     public Estoque() {
         instanciarComponentes();
@@ -35,7 +33,7 @@ public class Estoque implements BaseInterfaceJava {
 
     @Override
     public void gerarTela() {
-        jFrameEstoque = new JFrame("Cadastro de Estoque");
+        jFrameEstoque = new JFrame("Estoque");
         jFrameEstoque.setSize(800, 600);
         jFrameEstoque.setLayout(null);
         jFrameEstoque.setLocationRelativeTo(null);
@@ -45,73 +43,33 @@ public class Estoque implements BaseInterfaceJava {
 
     @Override
     public void adicionarComponentes() {
-        //JLabel's
-        jFrameEstoque.add(jLabelQuantidade);
+       
+        //jLabel's
         jFrameEstoque.add(jLabelId);
-        jFrameEstoque.add(jLabelCategoria);
-        jFrameEstoque.add(jLabelStatus);
-        jFrameEstoque.add(jLabelPesquisa);
-
-        //JTextField's
-        jFrameEstoque.add(jTextFieldQuantidade);
-        jFrameEstoque.add(jTextFieldId);
-        jFrameEstoque.add(jTextFieldStatus);
-        jFrameEstoque.add(jTextFieldPesquisa);
-
-        //Button
-        jFrameEstoque.add(jButtonSair);
         
-        //ComboBox
+        //JTextField's
+        
         
        
     }
 
     @Override
     public void gerarLocalizacoes() {
-        //JLabel's
-        jLabelQuantidade.setLocation(10, 40);
-
-        //JTextField
+     jLabelId.setLocation(20,20);
     }
 
     @Override
     public void gerarDimensoes() {
         //JLabel's
-        jLabelQuantidade.setSize(100, 20);
         jLabelId.setSize(100,20);
-        jLabelCategoria.setSize(100,20);
-        jLabelStatus.setSize(100,20);
-        jLabelPesquisa.setSize(100,20);
-       
-        //JTextField's
-        jTextFieldQuantidade.setSize(100,20);
-        jTextFieldId.setSize(100,20);
-        jTextFieldStatus.setSize(100,20);
-        jTextFieldPesquisa.setSize(100,20);
+        
         
     }
 
     @Override
     public void instanciarComponentes() {
-        //JLabel's
-        jLabelQuantidade = new JLabel("Quantidade");
-        jLabelCategoria = new JLabel("Categoria");
-        jLabelId = new JLabel("Número");
-        jLabelStatus = new JLabel("Status da peça");
-        jLabelPesquisa = new JLabel("Pesquisa");
-
-        //JTextField's
-        jTextFieldQuantidade = new JTextField("");
-        jTextFieldStatus = new JTextField("");
-        jTextFieldId = new JTextField("");
-        jTextFieldPesquisa = new JTextField("");
-       
-        //ComboBox
-       
-        
-       
-
-    
+      jLabelId = new JLabel("Número");
+ 
     }
 
 }
