e77b43dfd16508017a66b892ce4c83d2b91746ac
Guilherme Bialas <guilhermmebialas31@gmail.com>
2018-09-17 10:23:34 -0300

    Cadastro cliente

--- Projeto_Final/src/view/CadastroCliente.java
+++ Projeto_Final/src/view/CadastroCliente.java
@@ -8,6 +8,7 @@ package view;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JOptionPane;
+import javax.swing.text.MaskFormatter;
 
 /**
  *
@@ -21,6 +22,8 @@ public class CadastroCliente extends javax.swing.JFrame {
     public CadastroCliente() {
         initComponents();
         acaoBotaoAdicionar();
+        mascaraCpf();
+        mascaraCnpj();
         
     }
 
@@ -41,7 +44,6 @@ public class CadastroCliente extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jTextFieldTelefone = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
-        jTextFieldCpf = new javax.swing.JTextField();
         jLabel4 = new javax.swing.JLabel();
         jTextFieldCep = new javax.swing.JTextField();
         jLabel6 = new javax.swing.JLabel();
@@ -51,16 +53,15 @@ public class CadastroCliente extends javax.swing.JFrame {
         jLabel9 = new javax.swing.JLabel();
         jLabel10 = new javax.swing.JLabel();
         jLabel11 = new javax.swing.JLabel();
-        jLabel12 = new javax.swing.JLabel();
         jTextFieldCidade = new javax.swing.JTextField();
-        jTextFieldLougradouro = new javax.swing.JTextField();
         jTextFieldEndereco = new javax.swing.JTextField();
         jLabel13 = new javax.swing.JLabel();
         jTextFieldNumero = new javax.swing.JTextField();
         jButtonSair = new javax.swing.JButton();
         jButtonAdicionar = new javax.swing.JButton();
         jLabel5 = new javax.swing.JLabel();
-        jTextFieldCnpj = new javax.swing.JTextField();
+        jFormattedTextFieldCpf = new javax.swing.JFormattedTextField();
+        jFormattedTextFieldCnpj = new javax.swing.JFormattedTextField();
 
         jTextField8.setText("jTextField8");
 
@@ -88,8 +89,6 @@ public class CadastroCliente extends javax.swing.JFrame {
 
         jLabel11.setText("Endereço");
 
-        jLabel12.setText("Lougradouro");
-
         jLabel13.setText("Número");
 
         jButtonSair.setText("Sair");
@@ -114,39 +113,42 @@ public class CadastroCliente extends javax.swing.JFrame {
                                 .addComponent(jTextFieldTelefone)
                                 .addComponent(jTextFieldNumero, javax.swing.GroupLayout.DEFAULT_SIZE, 100, Short.MAX_VALUE))
                             .addComponent(jLabel13))
-                        .addGap(33, 33, 33)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel4)
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                .addComponent(jTextFieldBairro, javax.swing.GroupLayout.DEFAULT_SIZE, 100, Short.MAX_VALUE)
-                                .addComponent(jLabel3)
-                                .addComponent(jTextFieldCep)
-                                .addComponent(jTextFieldCpf)
-                                .addComponent(jLabel8)))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(33, 33, 33)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel4)
+                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                        .addComponent(jTextFieldBairro, javax.swing.GroupLayout.DEFAULT_SIZE, 100, Short.MAX_VALUE)
+                                        .addComponent(jLabel3)
+                                        .addComponent(jTextFieldCep)
+                                        .addComponent(jLabel8))))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(33, 33, 33)
+                                .addComponent(jFormattedTextFieldCpf, javax.swing.GroupLayout.PREFERRED_SIZE, 107, javax.swing.GroupLayout.PREFERRED_SIZE)))
                         .addGap(18, 18, 18)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(jLabel12)
                             .addComponent(jLabel9)
                             .addComponent(jLabel6)
-                            .addComponent(jTextFieldEmail)
-                            .addComponent(jTextFieldCidade)
-                            .addComponent(jTextFieldLougradouro, javax.swing.GroupLayout.PREFERRED_SIZE, 118, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(0, 33, Short.MAX_VALUE))
+                            .addComponent(jTextFieldEmail, javax.swing.GroupLayout.DEFAULT_SIZE, 118, Short.MAX_VALUE)
+                            .addComponent(jTextFieldCidade))
+                        .addGap(0, 39, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jTextFieldEndereco, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addGroup(layout.createSequentialGroup()
                                 .addGap(6, 6, 6)
                                 .addComponent(jLabel11)))
-                        .addGap(30, 30, 30)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
+                                .addGap(30, 30, 30)
                                 .addComponent(jLabel10)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addComponent(jLabel5)
                                 .addGap(0, 0, Short.MAX_VALUE))
                             .addGroup(layout.createSequentialGroup()
-                                .addComponent(jTextFieldCnpj, javax.swing.GroupLayout.DEFAULT_SIZE, 101, Short.MAX_VALUE)
+                                .addGap(30, 30, 30)
+                                .addComponent(jFormattedTextFieldCnpj, javax.swing.GroupLayout.DEFAULT_SIZE, 114, Short.MAX_VALUE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addComponent(jButtonAdicionar)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -161,23 +163,20 @@ public class CadastroCliente extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel6)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jTextFieldEmail, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jTextFieldEmail, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jFormattedTextFieldCpf, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(18, 18, 18)
                         .addComponent(jLabel9)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jTextFieldCidade, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(20, 20, 20)
-                        .addComponent(jLabel12)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jTextFieldLougradouro, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(66, 66, 66))
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel1)
                             .addComponent(jLabel3))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jTextFieldNome, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jTextFieldCpf, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(jTextFieldNome, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(20, 20, 20)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel2)
@@ -203,7 +202,7 @@ public class CadastroCliente extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jTextFieldEndereco, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jTextFieldCnpj, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(jFormattedTextFieldCnpj, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jLabel10))
                     .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -250,25 +249,13 @@ public class CadastroCliente extends javax.swing.JFrame {
             }
         });
     }
-    
-    
-    
-    
-    
-    
+
     public void validacao(){
       if(jTextFieldNome.getText().length() <3){
           JOptionPane.showMessageDialog(null,
                     "O nome deve conter no mínimo três caracteres", "Cadastro Cliente",
                     JOptionPane.ERROR_MESSAGE);
-         jTextFieldNome.requestFocus();
-            return;
-      }
-      if(jTextFieldBairro.getText().length()<3){
-           JOptionPane.showMessageDialog(null,
-                    "O bairro deve conter no mínimo três caracteres", "Cadastro Cliente",
-                    JOptionPane.ERROR_MESSAGE);
-         jTextFieldBairro.requestFocus();
+         jTextFieldNome.requestFocus(); 
             return;
       }
       if(jTextFieldEmail.getText().length()<3){
@@ -278,14 +265,43 @@ public class CadastroCliente extends javax.swing.JFrame {
          jTextFieldEmail.requestFocus();
             return;
       }
-    if(jTextFieldEndereco.getText().length()<3){
+      if(jTextFieldBairro.getText().length()<3){
+           JOptionPane.showMessageDialog(null,
+                    "O bairro deve conter no mínimo três caracteres", "Cadastro Cliente",
+                    JOptionPane.ERROR_MESSAGE);
+         jTextFieldBairro.requestFocus();
+            return;
+      }
+     if(jTextFieldEndereco.getText().length()<3){
          JOptionPane.showMessageDialog(null,
                     "O e-mail deve conter no mínimo três caracteres", "Cadastro Cliente",
                     JOptionPane.ERROR_MESSAGE);
          jTextFieldEndereco.requestFocus();
             return;
     }
+    
+       
     }
+    
+    
+    public void mascaraCpf(){
+        try {
+             MaskFormatter maskFormatter = new MaskFormatter("###.###.###-##");
+             maskFormatter.install(jFormattedTextFieldCpf);
+        } catch (Exception e) {
+                  JOptionPane.showMessageDialog(null, "Chame o prog");
+        }
+    }
+    
+    public void mascaraCnpj(){
+        try {
+            MaskFormatter maskFormatter = new MaskFormatter("##.###./####-##");
+             maskFormatter.install(jFormattedTextFieldCnpj);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Chame o prog");
+        }
+    }
+  
      public void acaoBotaoAdicionar(){
          jButtonAdicionar.addActionListener(new ActionListener() {
 
@@ -312,10 +328,11 @@ public class CadastroCliente extends javax.swing.JFrame {
     private javax.swing.JButton jButton3;
     private javax.swing.JButton jButtonAdicionar;
     private javax.swing.JButton jButtonSair;
+    private javax.swing.JFormattedTextField jFormattedTextFieldCnpj;
+    private javax.swing.JFormattedTextField jFormattedTextFieldCpf;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel11;
-    private javax.swing.JLabel jLabel12;
     private javax.swing.JLabel jLabel13;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
@@ -328,11 +345,8 @@ public class CadastroCliente extends javax.swing.JFrame {
     private javax.swing.JTextField jTextFieldBairro;
     private javax.swing.JTextField jTextFieldCep;
     private javax.swing.JTextField jTextFieldCidade;
-    private javax.swing.JTextField jTextFieldCnpj;
-    private javax.swing.JTextField jTextFieldCpf;
     private javax.swing.JTextField jTextFieldEmail;
     private javax.swing.JTextField jTextFieldEndereco;
-    private javax.swing.JTextField jTextFieldLougradouro;
     private javax.swing.JTextField jTextFieldNome;
     private javax.swing.JTextField jTextFieldNumero;
     private javax.swing.JTextField jTextFieldTelefone;
